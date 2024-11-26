7294e86242998e79c1d349ec2ff40b1f3a98ccbc
Trevisoli517 <00001091551169sp@al.educacao.sp.gov.br>
2024-11-26 13:18:27 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'o que é um vetor?',
+    'Um vetor é uma lista ordenada de elementos do mesmo tipo, acessados por índices. Em matemática, é uma entidade com direção e magnitude.'
+)
+
+criaCartao(
+    'programação',
+    'O Que o printf"" faz em um codigo(linguagem em c)?',
+    'exibe caracteres na tela do usuário'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma faz o "scanf" em um codigo(linguagem em c) ?',
+    'recebe caracteres do usuário e armazena em determinada variavel indicada'
+)
+
+criaCartao(
+    'programação',
+    'o que é uma linguagem de programação?',
+    'Uma linguagem de programação é um conjunto de regras e comandos usados para criar programas de computador. Ela permite que humanos instruam a máquina a realizar tarefas específicas.'
+)
